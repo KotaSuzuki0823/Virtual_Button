a91b3c1a1919deccde3fd93fb765ed6b634f4b70
KotaSuzuki <kota.suzuki@ucl.meijo-u.ac.jp>
2020-12-11 22:14:03 +0900

    natureをクラス化

--- server/main/appliances/nature.go
+++ server/main/appliances/nature.go
@@ -1,19 +1,33 @@
 package appliances
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
-	"os"
 )
 
+type Nature struct {
+	token string
+}
+
+func (n Nature) SetToken() {
+	// tokenファイル読み込み
+	bytes, err := ioutil.ReadFile("token")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	n.token = string(bytes)
+}
+
 // SendSignal idで指定した赤外線シグナルの送信
-func SendSignal(id string) {
+func (n Nature) SendSignal(id string) {
 	url := "https://api.nature.global/1/signals/" + id + "/send"
 	req, _ := http.NewRequest("POST", url, nil)
 
 	req.Header.Set("accept", " application/json")
-	req.Header.Set("Authorization", " Bearer "+token)
+	req.Header.Set("Authorization", " Bearer "+n.token)
 
 	client := new(http.Client)
 	log.Printf("Send request")
